66ab377d233519d9c66e784280796c5d5c77ef08
paoloanchoriz <paolo.anchoriz@gmail.com>
2015-09-18 15:45:20 +0800

    Encapsulate script in a module that would run after all DOMs are loaded

--- lists/static/list.js
+++ lists/static/list.js
@@ -1,4 +1,9 @@
+(function($) {
+    $(document).ready(function() {
+        $('input').on('keypress', function() {
+            $('.has-error').hide();
+        });
+    });
+})(jQuery);
 
-$('input').on('keypress', function() {
-    $('.has-error').hide();
-});
+
