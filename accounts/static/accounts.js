dbcbd41c949b5453c103e3f23adceca22972525d
paoloanchoriz <paolo.anchoriz@gmail.com>
2015-09-21 16:05:46 +0800

    added sinon and persona include, and separate js for accounts

---
+++ accounts/static/accounts.js
@@ -0,0 +1,14 @@
+var initialize = function(navigator, user, token, urls) {
+    $('#id_login').on('click', function() {
+        navigator.id.request();
+    });
+    navigator.id.watch({
+        loggedInUser: user
+    });
+};
+
+window.Superlists = {
+    Accounts: {
+        initialize: initialize
+    }
+};
